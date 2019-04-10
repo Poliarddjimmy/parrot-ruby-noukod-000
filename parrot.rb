47ba2932c58240a49886f115125debfe7e935de3
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-10 13:37:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(name = "Squawk!")
-  name.each do |na|
-    na.each do |nan|
-      puts nan
-    end
-  end
+  
 end
 
 parrot("Pretty bird!")

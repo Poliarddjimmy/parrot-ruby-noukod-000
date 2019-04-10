a2ca18c03c59bf9ddf487ab0ada25064e110e2fa
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-10 13:41:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(name = "Squawk!")
-  if !name
-    puts name
-  end
-  
+  return 
 end
 
 parrot("Pretty bird!")
